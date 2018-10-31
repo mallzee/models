adb9f6f4ec3c138ef5b40f947f2a37f2968b8386
martinapugliese <m.letitbe@gmail.com>
2018-10-31 17:30:40 +0000

    chore(object detection): protocol buffer files, linted files and fixed viz warning

--- research/object_detection/export_tflite_ssd_graph.py
+++ research/object_detection/export_tflite_ssd_graph.py
@@ -117,21 +117,21 @@ FLAGS = flags.FLAGS
 
 
 def main(argv):
-  del argv  # Unused.
-  flags.mark_flag_as_required('output_directory')
-  flags.mark_flag_as_required('pipeline_config_path')
-  flags.mark_flag_as_required('trained_checkpoint_prefix')
+    del argv  # Unused.
+    flags.mark_flag_as_required('output_directory')
+    flags.mark_flag_as_required('pipeline_config_path')
+    flags.mark_flag_as_required('trained_checkpoint_prefix')
 
-  pipeline_config = pipeline_pb2.TrainEvalPipelineConfig()
+    pipeline_config = pipeline_pb2.TrainEvalPipelineConfig()
 
-  with tf.gfile.GFile(FLAGS.pipeline_config_path, 'r') as f:
-    text_format.Merge(f.read(), pipeline_config)
-  text_format.Merge(FLAGS.config_override, pipeline_config)
-  export_tflite_ssd_graph_lib.export_tflite_graph(
-      pipeline_config, FLAGS.trained_checkpoint_prefix, FLAGS.output_directory,
-      FLAGS.add_postprocessing_op, FLAGS.max_detections,
-      FLAGS.max_classes_per_detection)
+    with tf.gfile.GFile(FLAGS.pipeline_config_path, 'r') as f:
+        text_format.Merge(f.read(), pipeline_config)
+    text_format.Merge(FLAGS.config_override, pipeline_config)
+    export_tflite_ssd_graph_lib.export_tflite_graph(
+        pipeline_config, FLAGS.trained_checkpoint_prefix, FLAGS.output_directory,
+        FLAGS.add_postprocessing_op, FLAGS.max_detections,
+        FLAGS.max_classes_per_detection)
 
 
 if __name__ == '__main__':
-  tf.app.run(main)
+    tf.app.run(main)
---
+++ research/object_detection/train.sh
@@ -0,0 +1,6 @@
+python model_main.py \
+    --pipeline_config_path="/Users/martina/Dropbox (Mallzee)/shared_imgs/tops-bottoms-tf-trainingsets/training_folder/models/model/faster_rcnn_inception_v2_pets.config" \
+    --model_dir="/Users/martina/Dropbox (Mallzee)/shared_imgs/tops-bottoms-tf-trainingsets/training_folder/models/model" \
+    --num_train_steps=1 \
+    --sample_1_of_n_eval_examples=1 \
+    --alsologtostderr
